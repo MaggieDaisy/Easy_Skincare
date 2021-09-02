cfc87f5054b0ac17317bc5378c026ac9477f904f
MaggieDaisy <missgosia84@gmail.com>
2021-09-02 14:15:24 +0000

    Create home app, add templates, adjust settings and urls in home app and easy skincare app

--- easy_skincare/settings.py
+++ easy_skincare/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "home",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,14 @@ ROOT_URLCONF = "easy_skincare.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(
+                BASE_DIR,
+                "templates",
+                "allauth",
+            ),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

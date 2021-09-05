68e696c4e2743cf76dfbceb6e34ea6e65d41fd17
MaggieDaisy <missgosia84@gmail.com>
2021-09-05 15:31:29 +0000

    Add serch query to the products view, tested if functionality works in search bar

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 
 from .models import Product
 
@@ -8,9 +10,20 @@ def all_products(request):
     """A view to show all products, including sorting and search queries"""
 
     products = Product.objects.all()
+    query = None
+
+    if "q" in request.GET:
+        query = request.GET["q"]
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse("products"))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         "products": products,
+        "search_term": query,
     }
 
     return render(request, "products/products.html", context)

56b4b8ad55a525c44b571665ba24bf609c50f8bc
MaggieDaisy <missgosia84@gmail.com>
2021-09-05 22:02:54 +0000

    Add dynamic for source selector box to make it work on click, fix sorting products by name by importing lower to product viewe

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 
 from .models import Category, Product

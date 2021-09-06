993fe3a81dcc6af006e7844f301cdee8f3e1564e
MaggieDaisy <missgosia84@gmail.com>
2021-09-06 00:43:24 +0000

    Create contexts in bag app, add delivery calculation logic

--- easy_skincare/settings.py
+++ easy_skincare/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",  # required by allauth allows HTTP request
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "bag.contexts.bag_contents",
             ],
         },
     },
@@ -157,6 +158,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+FREE_DELIVERY_THRESHOLD = 25
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

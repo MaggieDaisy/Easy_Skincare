a93659e1496bf161a767f4b43fd8395884c00a79
MaggieDaisy <missgosia84@gmail.com>
2021-09-07 14:48:57 +0000

    Update context processor for quantity items in bag

--- easy_skincare/settings.py
+++ easy_skincare/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-FREE_DELIVERY_THRESHOLD = 25
+FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type

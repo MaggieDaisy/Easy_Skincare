d945927942c7c4f73124cc618311cae7ea33521e
MaggieDaisy <missgosia84@gmail.com>
2021-09-08 10:04:08 +0000

    Add toast messages to bag views

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,6 @@
 from django.contrib import messages
-from django.shortcuts import (
-    HttpResponse,
-    get_object_or_404,
-    redirect,
-    render,
-    reverse,
-)
+from django.shortcuts import (HttpResponse, get_object_or_404, redirect,
+                              render, reverse)
 from products.models import Product
 
 # Create your views here.
@@ -28,6 +23,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f"Added {product.name} to your bag")
@@ -40,13 +36,16 @@ def adjust_bag(request, item_id):
     """A view that allow users to adjust the quantity
     of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get("quantity"))
     bag = request.session.get("bag", {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
     request.session["bag"] = bag
     return redirect(reverse("view_bag"))
@@ -57,12 +56,15 @@ def remove_from_bag(request, item_id):
     the item from the shopping bag"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get("bag", {})
 
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
         request.session["bag"] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)

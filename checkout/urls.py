b0cee08a12c5d07741b70fdf98a4904f25f07543
MaggieDaisy <missgosia84@gmail.com>
2021-09-09 16:14:28 +0000

    Setup the webhook listener and test it out

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path("", views.checkout, name="checkout"),
@@ -9,4 +10,5 @@ urlpatterns = [
         views.checkout_success,
         name="checkout_success",
     ),
+    path("wh/", webhook, name="webhook"),
 ]

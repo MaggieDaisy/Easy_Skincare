4901207463aa32279189df0de7cfbe1b8c0e8bb1
MaggieDaisy <missgosia84@gmail.com>
2021-09-10 18:41:22 +0000

    Test the user profile registration and login process, display user name in profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import UserProfile
 
 
 def profile(request):
     """A view that display the user's profile"""
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = "profiles/profile.html"
-    context = {}
+    context = {
+        "profile": profile,
+    }
 
     return render(request, template, context)

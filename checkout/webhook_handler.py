4b99a1afef115c9496d50d3e008b38bf2cbdf0e7
MaggieDaisy <missgosia84@gmail.com>
2021-09-11 21:40:07 +0000

    Adjust webhook handler to handle user profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from products.models import Product
+from profiles.models import UserProfile
 
 from .models import Order, OrderLineItem
 
@@ -46,6 +47,21 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != "AnonymousUser":
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -80,6 +96,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -99,7 +116,6 @@ class StripeWH_Handler:
                         quantity=item_data,
                     )
                     order_line_item.save()
-
             except Exception as e:
                 if order:
                     order.delete()

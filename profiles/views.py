6db44c2646e4003dc3aa873e11d3e84224dd715f
MaggieDaisy <missgosia84@gmail.com>
2021-09-11 18:53:05 +0000

    Adjust country field in profile, add profile form, add view and update profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, render
 
+from .forms import UserProfileForm
 from .models import UserProfile
 
 
@@ -7,9 +9,20 @@ def profile(request):
     """A view that display the user's profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully")
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = "profiles/profile.html"
     context = {
+        "form": form,
         "profile": profile,
+        "orders": orders,
     }
 
     return render(request, template, context)

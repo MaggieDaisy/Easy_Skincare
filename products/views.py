c668b35eff9c1f3e0a44e4eb3ab1c6b863111ec2
MaggieDaisy <missgosia84@gmail.com>
2021-09-12 14:43:20 +0000

    Add functionality to add product for admin, adjust url for add product and test if adding product works for management

--- products/views.py
+++ products/views.py
@@ -71,7 +71,20 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """A view to add a product to the store"""
-    form = ProductForm()
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, "Successfully added product!")
+            return redirect(reverse("product_detail", args=[product.id]))
+        else:
+            messages.error(
+                request,
+                "Failed to add product. Please ensure the form is valid.",
+            )
+    else:
+        form = ProductForm()
+
     template = "products/add_product.html"
     context = {
         "form": form,
--- profiles/views.py
+++ profiles/views.py
@@ -15,16 +15,16 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Profile updated successfully")
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(
+                request, "Update failed. Please ensure the form is valid."
+            )
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = "profiles/profile.html"
-    context = {
-        "form": form,
-        "profile": profile,
-        "orders": orders,
-    }
+    context = {"form": form, "orders": orders, "on_profile_page": True}
 
     return render(request, template, context)
 

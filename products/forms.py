bc82f60a4cba7e9ffc9db24256c7841cbf442d76
MaggieDaisy <missgosia84@gmail.com>
2021-09-12 16:27:41 +0000

    Add custom vidget template to fix the image field for admin functionality

--- products/forms.py
+++ products/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 
 from .models import Category, Product
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
+
+    image = forms.ImageField(
+        label="Image", required=False, widget=CustomClearableFileInput
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)

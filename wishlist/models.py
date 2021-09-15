81580cae122aac32b3af15f827a6bbf49774433d
MaggieDaisy <missgosia84@gmail.com>
2021-09-15 13:23:54 +0000

    Create a wishlist app, add wishlist model

---
+++ wishlist/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
+
+
+class Wishlist(models.Model):
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders",
+    )
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE
+    )
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name

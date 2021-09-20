863d84417bc34fb5306a184e3cd67c609e043aa7
MaggieDaisy <missgosia84@gmail.com>
2021-09-20 15:14:27 +0000

    Enable automatic deployment to heroku, replace secret key settings, set debug for developmnet

--- easy_skincare/settings.py
+++ easy_skincare/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-m5mpo$^50z2!1r@jvnat#@w^(tq7p0$bxbg02tg&!@#(ldl4d("
-)
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["easy-skincare.herokuapp.com", "localhost"]
 

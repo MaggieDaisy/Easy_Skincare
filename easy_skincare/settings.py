4a92ae1f737c64a8baba6d40350c497fc4635502
MaggieDaisy <missgosia84@gmail.com>
2021-09-20 13:32:31 +0000

    Install gunicorn, freeze changes to requirements, create procfile to serve django app, disable static files, allow hosts

--- easy_skincare/settings.py
+++ easy_skincare/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["easy-skincare.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -126,12 +126,18 @@ WSGI_APPLICATION = "easy_skincare.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

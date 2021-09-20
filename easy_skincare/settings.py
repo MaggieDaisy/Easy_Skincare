53954e7c4b8f45b382191e7fbd24b11ea0ac76ce
MaggieDaisy <missgosia84@gmail.com>
2021-09-20 13:05:11 +0000

    Create heroku app, install dj database and psycopg2 binary, freeze requirements, import data base to main settings, connect to postgres, run migrations, load data for categories and products, create super user, remove config heroku db

--- easy_skincare/settings.py
+++ easy_skincare/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,7 +132,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
